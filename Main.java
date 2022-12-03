6b93e7a3a823c3912305b2835d48ac34828dba8f
Rebecca <rnwldms@gmail.com>
2022-12-02 23:20:10 -0600

    Save Strings

--- Main.java
+++ Main.java
@@ -1,25 +1,60 @@
 import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*;
+import java.util.ArrayList;  
 
-public class Main{
+public class Main  implements ActionListener{
     JFrame f;
+    JLabel l1,l2;  
+    JButton b;
+    JTextField tf;
+    JLabel l;
+    ArrayList<String> arr;
     Main(){
+        arr = new ArrayList<String>();
         f = new JFrame();
-        JButton b = new JButton("Click Here");
-        final JTextField tf = new JTextField();
-        tf.setBounds(50,50,150,20);
-        b.setBounds(130,100,100,40);
+        b = new JButton("Add");
+        tf= new JTextField();
+         
+        tf.setBounds(310,60,150,70);
+        b.setBounds(50,140,100,40);
+        b.addActionListener(this);       
+
+        l1=new JLabel("This is Java Swing Practice.");  
+        l1.setBounds(50,50, 300,30);  
+        l2=new JLabel("Leave a Message");  
+        l2.setBounds(50,100, 300,30);  
+
+        l = new JLabel("");
+        l.setBounds(50,190, 300,30);  
+        f.add(l1); f.add(l2);
+        f.add(l);
  
         f.add(b);
         f.add(tf);
 
         f.setSize(600,600);
         f.setLayout(null);
-        f.setVisible(true);
-
-        
+        f.setVisible(true); 
     }
-    public static void main(String args[]){
-        // System.out.println("Hi");
+
+    public void actionPerformed(ActionEvent e) {  
+        try{
+            String text = tf.getText();
+            arr.add(text);
+            
+            String msg="";
+
+            for (int i = 0; i < arr.size(); i++) {
+                msg += (arr.get(i)+System.lineSeparator());
+
+              }
+              l.setText("Your Message: "+msg);  
+        }catch(Exception ex){System.out.println(ex);}  
+    }  
+
+
+    public static void main(String args[]){ 
         new Main();
     }
 }
