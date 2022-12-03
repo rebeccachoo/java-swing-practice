8c8d57fbb8305d92998c43b7db1a0370a52902d8
Rebecca <rnwldms@gmail.com>
2022-12-02 22:57:24 -0600

    JTextField Added

--- Main.java
+++ Main.java
@@ -4,13 +4,19 @@ public class Main{
     JFrame f;
     Main(){
         f = new JFrame();
-        JButton b = new JButton("Click");
+        JButton b = new JButton("Click Here");
+        final JTextField tf = new JTextField();
+        tf.setBounds(50,50,150,20);
         b.setBounds(130,100,100,40);
+ 
         f.add(b);
+        f.add(tf);
 
         f.setSize(600,600);
         f.setLayout(null);
         f.setVisible(true);
+
+        
     }
     public static void main(String args[]){
         // System.out.println("Hi");
